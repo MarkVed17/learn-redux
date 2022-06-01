85137888e2d1611e097fc6382b5bf73c64ebc451
MarkVed17 <vedantlahane17@gmail.com>
2022-06-01 23:10:02 +0530

    feat: added `restock-cake` action

--- index.js
+++ index.js
@@ -2,13 +2,21 @@ const redux = require("redux"); // import redux
 const createStore = redux.createStore; // import the createStore redux method
 
 const CAKE_ORDERED = "CAKE_ORDERED"; // string constant
+const CAKE_RESTOCKED = "CAKE_RESTOCKED";
 
 // Action Creator
 function orderCake() {
   return {
     // defining an action
     type: CAKE_ORDERED,
-    quantity: 1,
+    payload: 1,
+  };
+}
+
+function restockCake(qty = 1) {
+  return {
+    type: CAKE_RESTOCKED,
+    payload: qty,
   };
 }
 
@@ -25,6 +33,11 @@ const reducer = (state = inititalState, action) => {
         ...state, // spread operator to make a copy of all the properties
         numberOfCakes: state.numberOfCakes - 1,
       };
+    case CAKE_RESTOCKED:
+      return {
+        ...state,
+        numberOfCakes: state.numberOfCakes + action.payload,
+      };
     default:
       return state;
   }
@@ -36,14 +49,16 @@ const reducer = (state = inititalState, action) => {
 const store = createStore(reducer); // create a redux store
 console.log("Initial state", store.getState()); // allows access to the current state of the application
 
-const unsubscribe = store.subscribe(() => console.log("Update", store.getState())); // register even listeners using subscribe()
+const unsubscribe = store.subscribe(() =>
+  console.log("Update", store.getState())
+); // register even listeners using subscribe()
 
-store.dispatch(orderCake()) // dispatch function takes an action creator function which in turn invokes an action
-store.dispatch(orderCake())
-store.dispatch(orderCake())
+store.dispatch(orderCake()); // dispatch function takes an action creator function which in turn invokes an action
+store.dispatch(orderCake());
+store.dispatch(orderCake());
+store.dispatch(restockCake(3));
 
-
-unsubscribe()
+unsubscribe();
 // None of the dispatches would work below the unsubscribe method
 
-store.dispatch(orderCake()) //This whon't be logged
+store.dispatch(orderCake()); //This whon't be logged
