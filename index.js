71ce2024833729c2299830029bf9d152b11a54d3
MarkVed17 <vedantlahane17@gmail.com>
2022-06-01 17:04:26 +0530

    feat: added `actions`

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log("From index.js");
+const CAKE_ORDERED = "CAKE_ORDERED"; // string constant
+
+// Action Creator
+function orderCake() {
+  return {
+    // defining an action
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
+}
