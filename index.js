95945ee53552abc63baebf174142c20a59caab9a
MarkVed17 <vedantlahane17@gmail.com>
2022-06-01 22:49:46 +0530

    feat: added `store`

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const redux = require("redux"); // import redux
+const createStore = redux.createStore; // import the createStore redux method
+
 const CAKE_ORDERED = "CAKE_ORDERED"; // string constant
 
 // Action Creator
@@ -26,3 +29,21 @@ const reducer = (state = inititalState, action) => {
       return state;
   }
 };
+
+// createStore takes an argument i.e. a reducer function,
+// And the reducer function itself has state as an argument.
+// This is how the redux store holds the application state.
+const store = createStore(reducer); // create a redux store
+console.log("Initial state", store.getState()); // allows access to the current state of the application
+
+const unsubscribe = store.subscribe(() => console.log("Update", store.getState())); // register even listeners using subscribe()
+
+store.dispatch(orderCake()) // dispatch function takes an action creator function which in turn invokes an action
+store.dispatch(orderCake())
+store.dispatch(orderCake())
+
+
+unsubscribe()
+// None of the dispatches would work below the unsubscribe method
+
+store.dispatch(orderCake()) //This whon't be logged
