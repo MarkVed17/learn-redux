17cb5ad4b4f1ff71bf47b4547385e1678f3f07b5
MarkVed17 <vedantlahane17@gmail.com>
2022-06-02 21:06:51 +0530

    feat: added a `cakeSlice`

---
+++ redux-toolkit-demo/features/cake/cakeSlice.js
@@ -0,0 +1,22 @@
+const createSlice = require("@reduxjs/toolkit").createSlice; // ES Module import
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+  name: cake,
+  initialState: initialState,
+  // initialState,                                  This is a ES6 shorthand
+  reducers: {
+    ordered: (state) => {
+      state.numberOfCakes--;
+    },
+    restocked: (state, action) => {
+      state.numberOfCakes += action.payload;
+    },
+  },
+});
+
+module.exports = cakeSlice.reducer                  // default export
+module.exports.cakeActions = cakeSlice.actions      // named export
