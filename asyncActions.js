8d5a1fe29829e32284e58c09ae8d03ff7743176b
MarkVed17 <vedantlahane17@gmail.com>
2022-06-02 15:22:43 +0530

    feat: network calls using `axios` & `redux-thunk`

--- asyncActions.js
+++ asyncActions.js
@@ -1,5 +1,8 @@
-const redux = require("redux")
-const createStore = redux.createStore
+const redux = require("redux");
+const thunkMiddleware = require("redux-thunk").default;
+const axios = require("axios");
+const createStore = redux.createStore;
+const applyMiddleware = redux.applyMiddleware;
 
 const initialState = {
   loading: false,
@@ -17,13 +20,13 @@ const fetchUsersRequest = () => {
   };
 };
 
-const fetchUsersSuccess = () => {
+const fetchUsersSuccess = (users) => {
   return {
     type: FETCH_USERS_SUCCEEDED,
     payload: users,
   };
 };
-const fetchUsersFailure = () => {
+const fetchUsersFailure = (error) => {
   return {
     type: FETCH_USERS_FAILED,
     payload: error,
@@ -41,6 +44,8 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         loading: false,
+        users: action.payload,
+        error: "",
       };
     case FETCH_USERS_FAILED:
       return {
@@ -51,4 +56,23 @@ const reducer = (state = initialState, action) => {
   }
 };
 
-const store = createStore(reducer)
+const fetchUsers = () => {
+  return function (dispatch) {
+    dispatch(fetchUsersRequest);
+    axios
+      .get("https://jsonplaceholder.typicode.com/users")
+      .then((response) => {
+        // response.data is the users
+        const users = response.data.map((user) => user.id);
+        dispatch(fetchUsersSuccess(users));
+      })
+      .catch((error) => {
+        // error.message is the error message
+        dispatch(fetchUsersFailure(error.message));
+      });
+  };
+};
+
+const store = createStore(reducer, applyMiddleware(thunkMiddleware));
+store.subscribe(() => console.log(store.getState()));
+store.dispatch(fetchUsers());
