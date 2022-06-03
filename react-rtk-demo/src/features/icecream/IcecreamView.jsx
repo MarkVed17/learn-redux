1c54acc446a5f4bb412c8919936a8d5760d62452
MarkVed17 <vedantlahane17@gmail.com>
2022-06-03 21:23:50 +0530

    feat: added views

---
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const IcecreamView = () => {
+  return (
+    <div>
+        <h2>Number of icecream - </h2>
+        <button>Order cake</button>
+        <button>Restock icecream</button>
+    </div>
+  )
+}
