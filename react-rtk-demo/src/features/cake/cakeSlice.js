4929b4845451bbdf3a20da3a52b30f834af0b67f
MarkVed17 <vedantlahane17@gmail.com>
2022-06-03 21:23:35 +0530

    feat: added slices

---
+++ react-rtk-demo/src/features/cake/cakeSlice.js
@@ -0,0 +1,22 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  numberOfCakes: 10,
+};
+
+const cakeSlice = createSlice({
+  name: "cake",
+  initialState: initialState,
+  // initialState,                                  This is a ES6 shorthand
+  reducers: {
+    ordered: (state) => {
+      state.numberOfCakes--;
+    },
+    restocked: (state, action) => {
+      state.numberOfCakes += action.payload;
+    },
+  },
+});
+
+export default cakeSlice.reducer; // default export
+export const {ordered, restocked} = cakeSlice.actions; // named export
---
+++ react-rtk-demo/src/features/icecream/icecreamSlice.js
@@ -0,0 +1,26 @@
+import { createSlice } from "@reduxjs/toolkit";
+import { ordered as cakeOrdered } from "../cake/cakeSlice";
+const initialState = {
+  numberOfIcecreams: 20,
+};
+
+const icecreamSlice = createSlice({
+  name: "icecream",
+  initialState,
+  reducers: {
+    ordered: (state) => {
+      state.numberOfIcecreams--;
+    },
+    restocked: (state, action) => {
+      state.numberOfIcecreams += action.payload;
+    },
+  },
+  extraReducers: (builder) => {
+    builder.addCase(cakeOrdered, (state) => {
+      state.numberOfIcecreams--;
+    });
+  },
+});
+
+module.exports = icecreamSlice.reducer;
+export const { ordered, restocked } = icecreamSlice.actions;
