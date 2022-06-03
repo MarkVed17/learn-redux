ff1c9e2f0b0c72f32232e5ef3d6bffa44f42a260
MarkVed17 <vedantlahane17@gmail.com>
2022-06-03 13:00:03 +0530

    trigger `fetchUsers` action from the store

--- redux-toolkit-demo/index.js
+++ redux-toolkit-demo/index.js
@@ -1,19 +1,23 @@
 const store = require("./app/store");
 const cakeActions = require("./features/cake/cakeSlice").cakeActions;
-const icecreamActions = require("./features/icecream/icecreamSlice").icecreamActions
+const icecreamActions =
+  require("./features/icecream/icecreamSlice").icecreamActions;
+const fetchUsers = require("./features/user/userSlice").fetchUsers;
 
 console.log("Initial State", store.getState());
 const unsubscribe = store.subscribe(() => {
-//   console.log("Update State", store.getState());    We now have a logger middleware attached to the store
+    console.log("Update State", store.getState());   
 });
 
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.ordered());
-store.dispatch(cakeActions.restocked(3));
+store.dispatch(fetchUsers());
 
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.ordered());
-store.dispatch(icecreamActions.restocked(2));
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.ordered());
+// store.dispatch(cakeActions.restocked(3));
 
-unsubscribe();
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.ordered());
+// store.dispatch(icecreamActions.restocked(2));
+
+// unsubscribe();
