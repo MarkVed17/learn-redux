ed432990b54babd4dff9518852454a18841333b2
MarkVed17 <vedantlahane17@gmail.com>
2022-06-03 22:18:11 +0530

    feat: rendered using `useSelector` hook

--- react-rtk-demo/src/features/cake/CakeView.jsx
+++ react-rtk-demo/src/features/cake/CakeView.jsx
@@ -1,9 +1,11 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 export const CakeView = () => {
+  const numberOfCakes = useSelector((state) => state.cake.numberOfCakes)
   return (
     <div>
-        <h2>Number of Cakes - </h2>
+        <h2>Number of Cakes - {numberOfCakes}</h2>
         <button>Order cake</button>
         <button>Restock cakes</button>
     </div>
--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,9 +1,12 @@
 import React from "react"
+import { useSelector } from "react-redux"
 
 export const IcecreamView = () => {
+  const numberOfIcecreams = useSelector((state) => state.icecream.numberOfIcecreams)
+
   return (
     <div>
-        <h2>Number of icecream - </h2>
+        <h2>Number of icecream - {numberOfIcecreams} </h2>
         <button>Order cake</button>
         <button>Restock icecream</button>
     </div>
