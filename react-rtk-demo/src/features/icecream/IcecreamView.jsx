93f2d7f9d6082fcdc42f8f60254cafb86f9f0e98
MarkVed17 <vedantlahane17@gmail.com>
2022-06-05 09:45:49 +0530

    feat: fired actions using `useDispatch` hook

--- react-rtk-demo/src/features/icecream/IcecreamView.jsx
+++ react-rtk-demo/src/features/icecream/IcecreamView.jsx
@@ -1,14 +1,19 @@
 import React from "react"
-import { useSelector } from "react-redux"
+import { useState } from "react"
+import { useSelector, useDispatch } from "react-redux"
+import { ordered, restocked } from "./icecreamSlice"
 
 export const IcecreamView = () => {
+  const [value, setValue] = useState(1)
   const numberOfIcecreams = useSelector((state) => state.icecream.numberOfIcecreams)
+  const dispatch = useDispatch()
 
   return (
     <div>
         <h2>Number of icecream - {numberOfIcecreams} </h2>
-        <button>Order cake</button>
-        <button>Restock icecream</button>
+        <button onClick={() => dispatch(ordered())}>Order cake</button>
+        <input type="number" value={value} onChange={(e) => setValue(parseInt(e.target.value))}/>
+        <button onClick={() => dispatch(restocked(value))}>Restock icecream</button>
     </div>
   )
 }
